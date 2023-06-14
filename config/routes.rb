d01c2e0fb9ee343aff89818ccea9caf17044f146
alexbotha <alexbotha@hotmail.co.uk>
2023-06-14 19:32:47 +0100

    adding resrouces - full crud for now

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  
+  resources :reviews
+  resources :tickets
+  resources :tours
+  resources :stadia
+  resources :grounds
 end
