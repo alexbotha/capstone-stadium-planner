00bc289d46fb0a97d54538b4b3782971a8be325b
alexbotha <alexbotha@hotmail.co.uk>
2023-06-15 11:30:11 +0100

    max capacity validation

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,6 +1,8 @@
 class Tour < ApplicationRecord
 
   validates :start_time, :end_time, presence: true
+  validates :max_capacity, numericality: { less_than_or_equal_to: 20 }
+
 
   has_many :tickets
   has_many :users, through: :tickets
