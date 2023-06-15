3a3a2c3dd9b9a700366ab5d8da0d45a4236fe423
alexbotha <alexbotha@hotmail.co.uk>
2023-06-15 10:58:02 +0100

    adding validate_length_of macro

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
 
   validates :review, presence: true
+  validates_length_of :review, minimum: 10, maximum: 1000, allow_blank: false
 
   belongs_to :user
   belongs_to :tour
