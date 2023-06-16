79fb820ddfa172ea24d33b78643c7c47a2f57d9e
alexbotha <alexbotha@hotmail.co.uk>
2023-06-16 15:47:36 +0100

    update to schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_14_172114) do
+ActiveRecord::Schema.define(version: 2023_06_16_133513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2023_06_14_172114) do
     t.string "about"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "country"
   end
 
   create_table "tickets", force: :cascade do |t|
