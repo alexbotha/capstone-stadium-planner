7a7a1f71635e2a434bc582c6c039c8681c51eaef
alexbotha <alexbotha@hotmail.co.uk>
2023-06-26 16:31:16 +0100

    login component update - form image url

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { UserContext } from "../context/user";
 import { useNavigate } from "react-router-dom";
 import { Container, Form, Button } from "react-bootstrap";
@@ -7,8 +7,22 @@ function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState([]);
-  const { login, loggedIn, user } = useContext(UserContext);
+  const [backgroundImage, setBackgroundImage] = useState("");
+  const { login, loggedIn } = useContext(UserContext);
   const navigate = useNavigate();
+
+  const images = [
+    "https://images.unsplash.com/photo-1577223645169-1e13f9056226?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80",
+    "https://images.unsplash.com/photo-1598121876853-82437626c783?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80",
+    "https://images.unsplash.com/photo-1665606462626-a3bc2f1f786b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80",
+    "https://images.unsplash.com/photo-1646491513192-60c3d88c04f2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1527&q=80",
+  ];
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImageUrl = images[randomIndex];
+    setBackgroundImage(`url(${randomImageUrl})`);
+  }, []);
 
   function signUp() {
     navigate("/signup");
@@ -38,35 +52,44 @@ function Login() {
   }
   if (!loggedIn) {
     return (
-      <div className="background-images">
-        <Container className="d-flex align-items-center justify-content-center vh-100">
-          <div>
-            <h3 className="pb-2">Welcome, to youTours</h3>
-            <Form className="form-style" onSubmit={handleSubmit}>
-              <Form.Control
-                type="text"
-                name="username"
-                value={username}
-                placeholder="Username"
-                onChange={(e) => setUsername(e.target.value)}
-              />
-              <Form.Control
-                type="password"
-                name="password"
-                value={password}
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
-              />
+      <div
+        style={{
+          backgroundImage: backgroundImage,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "600px 900px",
+          backgroundPosition: "top left",
+        }}
+      >
+        <div className="background-image-login">
+          <Container className="d-flex align-items-center justify-content-center vh-100">
+            <div>
+              <h3 className="pb-2">Welcome, to youTours</h3>
+              <Form className="form-style" onSubmit={handleSubmit}>
+                <Form.Control
+                  type="text"
+                  name="username"
+                  value={username}
+                  placeholder="Username"
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+                <Form.Control
+                  type="password"
+                  name="password"
+                  value={password}
+                  placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <div className="d-flex justify-content-center">
+                  <Form.Control type="submit" />
+                </div>
+                <ul className="error-text-color">{error}</ul>
+              </Form>
               <div className="d-flex justify-content-center">
-                <Form.Control type="submit" />
+                <Button onClick={signUp}>Click to register</Button>
               </div>
-              <ul className="error-text-color">{error}</ul>
-            </Form>
-            <div className="d-flex justify-content-center">
-              <Button onClick={signUp}>Click to register</Button>
             </div>
-          </div>
-        </Container>
+          </Container>
+        </div>
       </div>
     );
   } else {
