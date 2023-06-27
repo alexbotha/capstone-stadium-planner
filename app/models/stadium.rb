ac5929e613334182682a331f13c71b2e5faeacbf
alexbotha <alexbotha@hotmail.co.uk>
2023-06-27 15:08:35 +0100

    add validations and associations

--- app/models/stadium.rb
+++ app/models/stadium.rb
@@ -1,4 +1,7 @@
 class Stadium < ApplicationRecord
-
+  validates :name, :address, :image_url, :about, :rating, :country, presence: true
+ 
   has_many :tours
+  has_many :reviews, through: :tours
+
 end
