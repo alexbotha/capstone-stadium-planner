d8bfc1b65eab532149d401313d7b342e2cab6898
alexbotha <alexbotha@hotmail.co.uk>
2023-06-27 09:13:50 +0100

    routes rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,20 @@ Rails.application.routes.draw do
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
+
+  #Custom routes
+  post '/login', to: 'sessions#create' 
+  delete '/logout', to: 'sessions#destroy'
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show' 
+  patch '/users/:id', to: 'users#update'
   
   resources :reviews
   resources :tickets
   resources :tours
   resources :stadia
-  resources :grounds
+  resources :users
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
