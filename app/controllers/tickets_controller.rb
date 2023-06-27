b75a465feb055e36bb04b66a65309eec5a756842
alexbotha <alexbotha@hotmail.co.uk>
2023-06-27 10:35:54 +0100

    create method - fixing bugs

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -10,18 +10,18 @@ class TicketsController < ApplicationController
     render json: ticket, status: :ok
   end 
 
-  def create 
-    ticket = current_user.tickets.create(ticket_params)
-    if ticket.valid?
-      render json: ticket, status: :created 
-    else 
+  def create
+    ticket = current_user.tickets.build(ticket_params)
+    if ticket.save
+      render json: ticket, status: :created
+    else
       if ticket.errors[:base].present?
-        render json: {error: "Maximum capacity of 20 reached."}, status: :unprocessable_entity
-      else 
-      render json: {errors: ticket.errors.full_messages}, status: :unprocessable_entity
+        render json: { errors: ticket.errors[:base] }, status: :unprocessable_entity
+      else
+        render json: { errors: ticket.errors.full_messages }, status: :unprocessable_entity
       end
-    end 
-  end 
+    end
+  end
 
   def update 
     ticket = current_user.tickets.find(params[:id])
@@ -37,7 +37,7 @@ class TicketsController < ApplicationController
   end
   end 
 
-  def delete 
+  def destroy 
     ticket = find_ticket
     if ticket.user_id == session[:user_id]
       ticket.destroy 
@@ -53,7 +53,7 @@ class TicketsController < ApplicationController
   end
 
   def ticket_params
-    params.permit(:user_id, :id, :tour_id, :quantity)
+    params.permit(:user_id, :tour_id, :quantity, :id)
   end
 
   def find_ticket
@@ -61,3 +61,15 @@ class TicketsController < ApplicationController
   end 
  
 end
+
+# Using build will instantiate a new object in memory but does not persist this object to the database. 
+
+# ticket.save checks if the new ticket instance can be saved successfully. The save method attempts to save the instance to the db and returns 'true' if the save operation is successful or 'false' if there any any validation errors or other issues preventing it being saved to the db 
+
+# If ticket.save is false, we enter the else. We have additional conditional check. This checks if there any any errors specifically added to the :base attribute of the ticket instance. These are added in the custom validation 
+
+# If there are no errors in the :base attribute it means there are other validation errors not related to :base. In this case, we render json with "full.error.messages" from the instance. 
+
+# Build provides us more control. 
+
+# When we use create, it persists an object to the database instantly. If any validations are not met, the object is not persisted. This means our custom methods are never ran, because our validations fail and an error is raised. Likewise, if they do pass, then it's saved to the db. Our custom methods run, but it's now too late. The instance has already been saved. That is why we use .build because it allows us to run ALL of our validations actrec/custom BEFORE it's saved.
