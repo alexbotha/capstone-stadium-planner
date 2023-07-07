66cf71eee09b81405e980a34e37e9e68768b8c7c
alexbotha <alexbotha@hotmail.co.uk>
2023-07-07 16:07:44 +0100

    custom method - average rating

--- app/serializers/stadium_serializer.rb
+++ app/serializers/stadium_serializer.rb
@@ -1,7 +1,10 @@
 class StadiumSerializer < ActiveModel::Serializer
-  attributes :id, :name, :address, :image_url, :rating, :about, :country, :tours
+  attributes :id, :name, :address, :image_url, :rating, :about, :country, :tours, :reviews, :average_rating
 
- 
+ has_many :reviews, through: :tours
   
+ def average_rating 
+  object.reviews.average(:rating).to_f 
+end 
      
 end
