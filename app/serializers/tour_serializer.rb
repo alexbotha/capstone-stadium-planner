7f53eecaa213d611e95501140dc3ada9517aa115
alexbotha <alexbotha@hotmail.co.uk>
2023-07-07 11:01:20 +0100

    tour attributes serializer added

--- app/serializers/tour_serializer.rb
+++ app/serializers/tour_serializer.rb
@@ -1,3 +1,6 @@
 class TourSerializer < ActiveModel::Serializer
-  attributes :id, :max_capacity, :start_time, :tour_date, :rating, :stadium_id
+  attributes :id, :max_capacity, :start_time, :tour_date, :rating, :stadium
+
+  has_many :reviews
+
 end
