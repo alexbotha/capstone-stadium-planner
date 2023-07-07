8a18cb75e3253f54c95005cd209b813c71fc2bbf
alexbotha <alexbotha@hotmail.co.uk>
2023-07-07 10:59:12 +0100

    private method update

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,7 @@
 class Ticket < ApplicationRecord
 
-  validates :quantity, presence: true
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
+
 
   validate :validate_max_capacity
 
@@ -9,13 +10,24 @@ class Ticket < ApplicationRecord
 
   private 
 
-# Model cannot directly render a JSON response. Model not responsible for handling HTTP responses. Instead we add an error to the models generic 'base' attribute which is used for adding errors to the model object itself
 def validate_max_capacity
   total_booked_tickets = tour.tickets.sum(:quantity)
-  if total_booked_tickets > tour.max_capacity
-    errors.add(:base, "Tour has reached maximum capacity")
+  if total_booked_tickets + quantity.to_i > tour.max_capacity
+    errors.add(:quantity, "of tour has reached maximum capacity")
   end
 end
 
-
 end
+
+# Model cannot directly render a JSON response. Model not responsible for handling HTTP responses. Instead we add an error to the models generic 'base' attribute which is used for adding errors to the model object itself
+
+# Create a variable called total_booked_tickets
+
+# Calculate the total quantity of tickets booked for a tour by summing up the quantity attribute of all existing tickets. Tour => Tickets => Take the quantity of each ticket instance => sum the values from quantity up
+
+# Compare the sum of exisiting tickets with the quantity of the current ticket being created - quantity.to_i 
+
+# Run some logic to see if that is greater than the tour.max_capacity.
+
+# If that is the case add an error to the quantity attribute 
+
