c3dd44d583ae77d1e89b0f448be7a572db8d1674
alexbotha <alexbotha@hotmail.co.uk>
2023-07-10 19:02:59 +0100

    routes update

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,6 +9,7 @@ import NavBar from "./NavBar";
 import StadiumContainer from "./StadiumContainer";
 import Stadium from "./Stadium";
 import MyAccount from "./MyAccount";
+import UserTours from "./UserTours";
 
 function App() {
   const { loggedIn, user } = useContext(UserContext);
@@ -34,6 +35,17 @@ function App() {
         <Route exact path="/myaccount" element={<MyAccount />} />
         <Route exact path="/stadiums" element={<StadiumContainer />} />
         <Route exact path="/stadiums/:id" element={<Stadium users={users} />} />
+        <Route
+          exact
+          path="/users/:id"
+          element={users.length > 0 ? <UserTours users={users} /> : null}
+        />
+
+        <Route
+          exact
+          path="*"
+          element={<h3 className="errorHandle">404 page not found</h3>}
+        />
       </Routes>
     </div>
   );
