d129ed930c28f4e9e6606b0ce86ec4e1626addae
alexbotha <alexbotha@hotmail.co.uk>
2023-07-10 19:20:17 +0100

    custom method created

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,3 +1,9 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :tour_id, :review
+  attributes :id, :user_id, :tour_id, :review, :user_username, :rating, :stadium
+
+  def user_username
+    object.user.username
+  end
+
+ 
 end
