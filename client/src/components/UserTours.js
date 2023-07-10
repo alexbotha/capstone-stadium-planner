803680192d42aa0a9e835537bee9828fd5ee996e
alexbotha <alexbotha@hotmail.co.uk>
2023-07-10 19:00:43 +0100

    user tours component

---
+++ client/src/components/UserTours.js
@@ -0,0 +1,58 @@
+import React, { useContext } from "react";
+import { UserContext } from "../context/user";
+import { useParams, Link } from "react-router-dom";
+import { Card, Button, Col, Row } from "react-bootstrap";
+
+function UserTours({ users }) {
+  const { loggedIn, loading } = useContext(UserContext);
+  const params = useParams();
+
+  if (loggedIn) {
+    const foundUser = users.find(({ id }) => id === parseInt(params.id));
+
+    console.log(foundUser.tours);
+
+    return loading ? (
+      <h3 className="loading">Loading...</h3>
+    ) : (
+      <div className="my-account-details">
+        <h3>
+          You're viewing the stadium tours booked for - {""}
+          <em className="user-tour-name">{foundUser.username}</em>
+        </h3>
+        <Row>
+          {foundUser.tours.map((tour) => {
+            return (
+              <Col md={3} sm={6} xs={12} className="mb-4">
+                <Card className="modern-card">
+                  <div className="card-image">
+                    <Card.Img
+                      variant="top"
+                      src={tour.stadium.image_url}
+                      alt="stadium image"
+                    />
+                  </div>
+                  <Card.Body className="stadium-card-center">
+                    <Link to={`/stadiums/${tour.stadium.id}`}>
+                      {tour.stadium.name}
+                    </Link>
+                    <p>Date: {tour.tour_date}</p>
+                    <p></p>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+      </div>
+    );
+  } else {
+    return (
+      <div className="errorHandle">
+        <h3>Please log in or sign up</h3>
+      </div>
+    );
+  }
+}
+
+export default UserTours;
