96c597c1fb6fdfb330d1aad0c03597616baf4051
alexbotha <alexbotha@hotmail.co.uk>
2023-07-10 19:00:27 +0100

    upcoming and previous component btns

--- client/src/components/MyAccount.js
+++ client/src/components/MyAccount.js
@@ -2,12 +2,12 @@ import React, { useContext, useState } from "react";
 import { UserContext } from "../context/user";
 
 import { Card, Button, Col, Row } from "react-bootstrap";
-import CreateReview from "./CreateReview";
+
+import UpcomingTours from "./UpcomingTours";
+import PreviousTours from "./PreviousTours";
 
 function MyAccount() {
-  const [stadium, setStadium] = useState(null);
-
- 
+  const [upComingTours, setUpcomingTours] = useState(true);
 
   const { loggedIn, user, loading, stadiums, setStadiums, error } =
     useContext(UserContext);
@@ -17,46 +17,11 @@ function MyAccount() {
       <h3>Loading...</h3>
     ) : (
       <div>
-        <h3 className="stadium-card-center">{user.username} stadiums booked</h3>
-        <Row>
-          {user.tours.map((tour) => (
-            <>
-              <Col md={3} sm={6} xs={12} className="mb-4">
-                <Card className="modern-card">
-                  <div className="card-image">
-                    <Card.Img
-                      variant="top"
-                      src={tour.stadium.image_url}
-                      alt="stadium image"
-                    />
-                  </div>
-                  <Card.Body className="stadium-card-center">
-                    <Card.Title>{tour.stadium.name}</Card.Title>
-                    <Card.Title>Date: {tour.tour_date}</Card.Title>
-                    <Card.Title>Time: {tour.start_time}</Card.Title>
-                  </Card.Body>
+        <h3 className="stadium-card-center">Your Profile</h3>
+        <Button onClick={() => setUpcomingTours(true)}>Upcoming</Button>
+        <Button onClick={() => setUpcomingTours(false)}>Previous</Button>
 
-                  {stadium === null ? (
-                    <>
-                      <Button onClick={() => setStadium(tour)}>
-                        Create a review
-                      </Button>
-                    </>
-                  ) : (
-                    <div className="calendar-overlay">
-                      <div className="calendar-popup">
-                        <CreateReview
-                          setStadium={setStadium}
-                          stadium={stadium}
-                        />
-                      </div>
-                    </div>
-                  )}
-                </Card>
-              </Col>
-            </>
-          ))}
-        </Row>
+        {upComingTours ? <UpcomingTours /> : <PreviousTours />}
       </div>
     );
   } else {
@@ -67,5 +32,57 @@ function MyAccount() {
     );
   }
 }
+
+/* {upComingTours ? (
+          <Row>
+            {user.tours.map((tour) => (
+              <>
+                <Col md={3} sm={6} xs={12} className="mb-4">
+                  <Card className="modern-card">
+                    <div className="card-image">
+                      <Card.Img
+                        variant="top"
+                        src={tour.stadium.image_url}
+                        alt="stadium image"
+                      />
+                    </div>
+                    <Card.Body className="stadium-card-center">
+                      <Card.Title>{tour.stadium.name}</Card.Title>
+                      <Card.Title>Date: {tour.tour_date}</Card.Title>
+                      <Card.Title>Time: {tour.start_time}</Card.Title>
+                    </Card.Body>
+
+                    {stadium === null ? (
+                      <>
+                        <Button onClick={() => setStadium(tour)}>
+                          Create a review
+                        </Button>
+                      </>
+                    ) : (
+                      <div className="calendar-overlay">
+                        <div className="calendar-popup">
+                          <CreateReview
+                            setStadium={setStadium}
+                            stadium={stadium}
+                          />
+                        </div>
+                      </div>
+                    )}
+                  </Card>
+                </Col>
+              </>
+            ))}
+          </Row>
+        ) : (
+          <h3>False</h3>
+        )}
+      </div>
+    );
+  } else {
+    return (
+      <div className="errorHandle">
+        <h3>{error}</h3>;
+      </div>
+    ); */
 
 export default MyAccount;
