2fac57c5afca941d5b761f8c5b69777895b37335
alexbotha <alexbotha@hotmail.co.uk>
2023-07-11 20:41:42 +0100

    edit ticket component created

--- client/src/components/EditTicket.js
+++ client/src/components/EditTicket.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Card, Button, Col, Row } from "react-bootstrap";
 
-function EditTicket() {
-  return <div>EditTicket</div>;
+function EditTicket({ setStadium, stadium }) {
+  return (
+    <>
+      <h3>Hello</h3>
+      <Button onClick={() => setStadium(null)}>Close</Button>
+    </>
+  );
 }
 
 export default EditTicket;
