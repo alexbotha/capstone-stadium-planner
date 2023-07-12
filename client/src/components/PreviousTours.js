e6ca56b0af1e8f80c988c7d94289ec312e893f32
alexbotha <alexbotha@hotmail.co.uk>
2023-07-12 20:17:15 +0100

    will render btn dependant on review

--- client/src/components/PreviousTours.js
+++ client/src/components/PreviousTours.js
@@ -5,65 +5,71 @@ import CreateReview from "./CreateReview";
 
 function PreviousTours() {
   const [tourState, setTourState] = useState(null);
-  const [x, setX] = useState([]);
+
   const { loggedIn, user, loading } = useContext(UserContext);
 
-  useEffect(() => {
-    setX(user.tours.filter((tour) => tour.upcoming_tours === false));
-  }, [user]);
-
-  console.log(user);
-
   if (loggedIn) {
-    // let previousTour = user.tours.filter((tour) =>
-    //   tour.reviews.some((review) => review)
-    // );
     return loading ? (
       <h3>Loading...</h3>
     ) : (
       <>
         <h3>Your previous tours</h3>
 
-        <Row>
-          {x.map((tour) => (
-            <>
-              <Col md={3} sm={6} xs={12} className="mb-4">
-                <Card className="modern-card">
-                  <div className="card-image">
-                    <Card.Img
-                      variant="top"
-                      src={tour.stadium.image_url}
-                      alt="stadium image"
-                    />
-                  </div>
-                  <Card.Body className="stadium-card-center">
-                    <Card.Title>{tour.stadium.name}</Card.Title>
-                    <Card.Title>Date: {tour.tour_date}</Card.Title>
-                    <Card.Title>Time: {tour.start_time}</Card.Title>
-                  </Card.Body>
-                  {/* .some is used to iterate over our tour.reviews array, and checks to see if a review object exists for each tour object. It returns a boolean value dependant on the outcome which is returned in the new array created when we mapped over user.tours */}
-                  {tour.reviews.some((review) => review) ? (
-                    <Button>Edit Review</Button>
-                  ) : (
-                    <Button onClick={() => setTourState(tour)}>
-                      Create Review
-                    </Button>
-                  )}
-                  {tourState === null ? null : (
-                    <div className="calendar-overlay">
-                      <div className="calendar-popup">
-                        <CreateReview
-                          setTourState={setTourState}
-                          tourState={tourState}
+        {user !== null ? (
+          <Row>
+            {user.tickets
+              .filter((ticket) => ticket.upcoming_tours === false)
+              .map((filteredTicket, index) => (
+                <>
+                  <Col md={3} sm={6} xs={12} className="mb-4" key={index}>
+                    <Card className="modern-card">
+                      <div className="card-image">
+                        <Card.Img
+                          variant="top"
+                          src={filteredTicket.stadium.image_url}
+                          alt="stadium image"
                         />
                       </div>
-                    </div>
-                  )}
-                </Card>
-              </Col>
-            </>
-          ))}
-        </Row>
+                      <Card.Body className="stadium-card-center">
+                        <Card.Title>{filteredTicket.stadium.name}</Card.Title>
+                        <Card.Title>
+                          Date: {filteredTicket.tour.tour_date}
+                        </Card.Title>
+                        <Card.Title>
+                          Time: {filteredTicket.tour.start_time}
+                        </Card.Title>
+                      </Card.Body>
+                      {/* .some is used to iterate over our tour.reviews array, and checks to see if a review object exists for each tour object. It returns a boolean value dependant on the outcome which is returned in the new array created when we mapped over user.tours */}
+                      {/* {const p = user.tours.map((tour) =>
+    tour.reviews.map((review) => review.review)
+  );} */}
+                      {user.reviews
+                        .map((review) => review.tour_id)
+                        .includes(filteredTicket.tour.id) ? (
+                        <Button>Edit Review</Button>
+                      ) : (
+                        <Button onClick={() => setTourState(filteredTicket)}>
+                          Create Review
+                        </Button>
+                      )}
+                      {tourState === null ? null : (
+                        <div className="calendar-overlay">
+                          <div className="calendar-popup">
+                            <CreateReview
+                              setTourState={setTourState}
+                              tourState={tourState}
+                            />
+                          </div>
+                        </div>
+                      )}
+                    </Card>
+                  </Col>
+                </>
+              ))}
+          </Row>
+        ) : (
+          <h3>not tours avail</h3>
+        )}
       </>
     );
   }
