71d53fe6dcee528ca0ea7fd6ba28f592d1db5d96
alexbotha <alexbotha@hotmail.co.uk>
2023-07-13 14:40:35 +0100

    set object function and drop down input added

--- client/src/components/StadiumContainer.js
+++ client/src/components/StadiumContainer.js
@@ -10,10 +10,13 @@ function StadiumContainer() {
   const [searchRating, setSearchRating] = useState("");
   const { loggedIn, stadiums, error, loading } = useContext(UserContext);
 
-  // filterStadiums recieves the stadium object to then allow us to write our logic
+  // Filter over stadiums and recieve the callback
+  const filteredStadiums = stadiums.filter(filterStadiums);
+
+  // Our call back function filterStadiums recieves the stadium object from filteredStadiums to then allow us to write our logic
   function filterStadiums(stadium) {
-    // If our state of searchCountry is an empty string ("") then evaluate as true. This essentially bypasses the filtering which means all the stadium objects are returned in our new array
-    // OR if our searchCountry is not empty, then return the stadium objects whos .country includes the value that searchCountry is currently set
+    // If searchCountry is empty - true - return all stadiums
+    // If searchCountry is not empty, then return the stadium objects whos .country includes the value that searchCountry is currently set
     const matchCountry =
       searchCountry === "" ||
       stadium.country.toLowerCase().startsWith(searchCountry.toLowerCase());
@@ -22,11 +25,23 @@ function StadiumContainer() {
       searchName === "" ||
       stadium.name.toLowerCase().includes(searchName.toLowerCase());
 
-    return matchCountry && matchName;
+    const matchRating =
+      searchRating === "" || stadium.average_rating === parseInt(searchRating);
+
+    return matchCountry && matchName && matchRating;
   }
 
-  // we filter over stadiums, and instead of doing something with our stadium object in our filteredStadiums variable, we pass a callback function called filterStadiums. This helps keep code organized and readable and allows for reusability
-  const filteredStadiums = stadiums.filter(filterStadiums);
+  //  Set constructor recieves our array and creates a new set object in memory containing non duplicated values.
+  // we call 'new' which will create a new instance of the set object in memory containing our non duplicated values.
+  // we can now interact with the instance and do stuff with it.
+  function getUniqueValues(array) {
+    return [...new Set(array)];
+  }
+
+  const uniqueCountries = getUniqueValues(
+    stadiums.map((stadium) => stadium.country)
+  );
+  const uniqueNames = getUniqueValues(stadiums.map((stadium) => stadium.name));
 
   if (loggedIn) {
     return loading ? (
@@ -43,24 +58,42 @@ function StadiumContainer() {
         </div>
         <Row>
           <Col className="input-col">
-            <Form.Control
+            <Form.Select
               type="text"
               value={searchCountry}
               onChange={(e) => setSearchCountry(e.target.value)}
-              placeholder="Country"
-            />
-            <Form.Control
+            >
+              <option value="">Select Country</option>
+              {uniqueCountries.map((country) => (
+                <option key={country} value={country}>
+                  {country}
+                </option>
+              ))}
+            </Form.Select>
+            <Form.Select
               type="text"
               value={searchRating}
               onChange={(e) => setSearchRating(e.target.value)}
               placeholder="Rating"
-            />
-            <Form.Control
+            >
+              <option value="">Select Rating</option>
+              {[0, 1, 2, 3, 4, 5].map((rating) => (
+                <option key={rating} value={rating}>
+                  {rating}
+                </option>
+              ))}
+            </Form.Select>
+
+            <Form.Select
               type="text"
               value={searchName}
               onChange={(e) => setSearchName(e.target.value)}
-              placeholder="Name"
-            />
+            >
+              <option value="">Select Name</option>
+              {uniqueNames.map((name) => (
+                <option value={name}>{name}</option>
+              ))}
+            </Form.Select>
           </Col>
         </Row>
         <Row>
