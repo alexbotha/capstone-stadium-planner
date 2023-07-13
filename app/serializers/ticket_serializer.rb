781a0618bee2a30fa962826669d824aaeb2f9295
alexbotha <alexbotha@hotmail.co.uk>
2023-07-13 14:41:56 +0100

    added custom method for upcoming tours

--- app/serializers/ticket_serializer.rb
+++ app/serializers/ticket_serializer.rb
@@ -1,3 +1,13 @@
 class TicketSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :tour_id, :quantity
+  attributes :id, :user_id, :tour_id, :quantity, :tour, :stadium, :upcoming_tours
+ 
+
+  belongs_to :tour
+  has_one :stadium, through: :tour
+  belongs_to :user
+  
+
+  def upcoming_tours
+    object.tour.tour_date > Date.today
+  end
 end
