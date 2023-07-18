c0d94904470e68eae6e0c16faea41a698c38fe15
alexbotha <alexbotha@hotmail.co.uk>
2023-07-18 18:36:40 +0100

    before action added

--- app/controllers/stadia_controller.rb
+++ app/controllers/stadia_controller.rb
@@ -1,4 +1,5 @@
 class StadiaController < ApplicationController
+  before_action :auth
 
 # list all stadiums
   def index 
@@ -7,14 +8,14 @@ class StadiaController < ApplicationController
   end 
 
 # Create a stadium
-  def create
-    stadium = Stadium.create(stadium_params)
-    if stadium.valid?
-      render json: stadium, status: :created 
-    else 
-      render json: {errors: stadium.errors.full_messages}, status: :unprocessable_entity
-    end  
-  end 
+  # def create
+  #   stadium = Stadium.create(stadium_params)
+  #   if stadium.valid?
+  #     render json: stadium, status: :created 
+  #   else 
+  #     render json: {errors: stadium.errors.full_messages}, status: :unprocessable_entity
+  #   end  
+  # end 
 
 # Show a specific stadium 
   def show 
@@ -22,11 +23,26 @@ class StadiaController < ApplicationController
     render json: stadium, status: :ok 
   end 
 
-private 
+  # def update 
+  #   stadium = Stadium.find_by(id: params[:id])
+  #   if stadium
+  #     stadium.update!(stadium_params)
+  #     render json: stadium, status: :ok
+  #   else
+  #     render json: {error: "Not authorized to edit these details"}, status: :unauthorized
+  #   end 
+  # end 
 
-  def stadium_params
-    params.permit(:name, :address, :image_url, :about)
+ private 
+
+  # def stadium_params
+  #   params.permit(:name, :address, :image_url, :about, :country, :rating)
+  # end 
+
+  def auth
+    return render json: {error: "Not authorized. Please log in or sign up"}, status: :unauthorized unless session.include? :user_id
   end 
+
 
 
 end
